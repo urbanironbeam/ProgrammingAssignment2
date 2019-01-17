fb1ddecf69d102647ffef064500c37dc25b0dcfd
410002083 <aron.birenbaum@ge.com>
2019-01-16 19:01:59 -0800

    Completed week 03 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Functions for creating a "special matrix" that can solve and cache its 
+# inverse.  
+#
+# Forked from: https://github.com/rdpeng/ProgrammingAssignment2
+#
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#
+# Creates a "special matrix" i.e. actually a list of functions the can be 
+# used for setting/getting a matrix and its inverse
+#
+makeCacheMatrix <- function(theMatrix = matrix()) {
+    
+    inversedMatrix <- NULL
+    
+    set <- function(y) {
+        theMatrix <<- y
+        inversedMatrix <<- NULL
+    }
+    
+    get <- function() {theMatrix}
+    
+    setInverse <- function(im) {inversedMatrix <<- im}
+    getInverse <- function() { inversedMatrix }
+    
+    # Return a list with all the methods
+    funcs <- list(set = set, get = get, 
+                  setInverse = setInverse, getInverse = getInverse)
+    invisible(funcs)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# 
+# Returns the inverse from a "special Matrix", including solving the inverse
+# if that hasn't already been done.
+# 
+cacheSolve <- function(specialMatrix, ...) {
+    inverseMatrix <- specialMatrix$getInverse()
+    if(!is.null(inverseMatrix) ) {
+        message("returning cached value for inverse matrix")
+    }
+    else {
+        inverseMatrix <- solve( specialMatrix$get(), ... )
+        specialMatrix$setInverse(inverseMatrix)
+    }
+    
+    # return the inversed matrix
+    return(inverseMatrix)
 }
